553f67125ff548913eaddc297ded351a890f230b
Soul181 <64364995+Soul181@users.noreply.github.com>
2021-03-27 16:22:35 +0300

    Update install.php

--- install.php
+++ install.php
@@ -1,11 +1,17 @@
 <?php
 include 'vars.php';
 date_default_timezone_set('Europe/Moscow'); // Устанавливаем часовой пояс
+mysqli_query("CREATE DATABASE $database"); // создаем новую БД имя 
+
+//CREATE USER 'novyi_polzovatel'@'localhost' IDENTIFIED BY 'parol';  // создание нового пользователя
+//GRANT ALL PRIVILEGES ON * . * TO 'novyi_polzovatel'@'localhost';   // неограниченый доступ у пользователя
+//FLUSH PRIVILEGES; // обновить, чтобы изменения вступили в силу
+
 
 $conn = new mysqli($servername, $username, $password); // Создаем новый класс mysqli
-if ($conn) // если соединение с БД, то 
+if ($conn) // если успешно, то 
 	{
-    mysqli_query($conn, "CREATE DATABASE $database"); // создаем новую БД имя 
+    
 	mysqli_select_db($conn, "$database"); // выбираем эту БД
 	mysqli_query($conn, "CREATE TABLE `$table_name`(`longurl` text NOT NULL, `shorturl` text NOT NULL, `time` int NOT NULL)"); // создаем таблицу
 	mysqli_query($conn, "ALTER TABLE `$table_name` ADD PRIMARY KEY (`shorturl`(6))"); // присвоение столбцу короткий статуc ГЛАВНОГО 
